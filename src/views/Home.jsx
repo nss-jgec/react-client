d949f61800b26a966203d87ba1c5372e0bc57b64
sarthakroy107 <sarthakroy107@gmail.com>
2023-07-08 15:33:16 +0530

    Added sections to home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,22 @@
 import { Carousel, Timeline } from "flowbite-react";
 import Bullets from "../comps/TimelineBullets";
 import data from "../utils/HomeData";
+import Testimonials from "../comps/Testimonials";
+import LatestBlogs from "../comps/LatestBlogs";
+import OurJourney from "../comps/OurJourney";
+import Conclave from "../comps/Conclave";
+import GoadandVision from "../comps/GoadandVision";
+
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="w-11/12 m-auto h-72 md:h-96 rounded-none">
+      {/* <div className="w-11/12 m-auto h-72 md:h-96 rounded-none">
         <Carousel indicators={false}>
           <div className="bg-orange-700 h-96"></div>
           <div className="bg-green-500 h-96"></div>
           <div className="bg-blue-500 h-96"></div>
         </Carousel>
-      </div>
+      </div> */}
       <div
         className="flex flex-col items-center lg:flex-row justify-evenly m-4 p-2"
         id="about"
@@ -37,6 +43,11 @@ export default function Home() {
           <Bullets points={data["Logo"]} />
         </div>
       </div>
+      <GoadandVision/>
+      <Conclave/>
+      <OurJourney/>
+      <LatestBlogs/>
+      <Testimonials/>
     </div>
   );
 }
